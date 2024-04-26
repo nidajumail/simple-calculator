558bc7ba63cfa041bea617a77f59099a3477aa68
nidajumail <nidahaq17@gmail.com>
2024-04-26 00:29:08 -0700

    tweak the text

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter  your second number", type: "number", name: "secondNumber" },
     { message: "select one of the operator to perform action", type: "list", name: "operator", choices: ["Addition", "Subtraction", "Multiplication", "Division"], },
 ]);
 //conditional statement
